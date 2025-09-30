83576fd7d8121c3fb5e33daa4bcf8186d78426f3
Guilherme Motta <guizaosafe@gmail.com>
2025-09-30 19:45:05 -0300

    Campo de telefone
    .

--- public/admin.js
+++ public/admin.js
@@ -215,19 +215,23 @@ function handleError(error) {
 }
 
 function formatPhone(value) {
-  const digits = value.replace(/\D/g, '').slice(0, 11);
+    const digits = value.replace(/\D/g, '').slice(0, 11);
 
-  if (digits.length > 6) {
-    return `(${digits.slice(0, 2)}) ${digits.slice(2, 7)}-${digits.slice(7)}`;
-  }
-  if (digits.length > 2) {
-    return `(${digits.slice(0, 2)}) ${digits.slice(2)}`;
-  }
-  if (digits.length > 0) {
-    return `(${digits}`;
-  }
-  return '';
+    if (digits.length > 7) {
+        return `(${digits.slice(0, 2)}) ${digits.slice(2, 3)} ${digits.slice(3, 7)}-${digits.slice(7)}`;
+    }
+    if (digits.length > 6) {
+        return `(${digits.slice(0, 2)}) ${digits.slice(2, 3)} ${digits.slice(3, 7)}-`;
+    }
+    if (digits.length > 2) {
+       return `(${digits.slice(0, 2)}) ${digits.slice(2)}`;
+    }
+    if (digits.length > 0) {
+        return `(${digits}`;
+    }
+    return '';
 }
+
 
 function handlePhoneInput(event) {
     event.target.value = formatPhone(event.target.value);
